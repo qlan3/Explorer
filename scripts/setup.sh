7c349611bbc0300b07cd2b50c031feee40d923ce
qlan3 <lancelqf@gmail.com>
2019-06-17 11:09:32 -0600

    fix some bug

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Load singularity
-module load singularity/2.5
+module load singularity/2.6
 
 # Pull the image (if not already exists)
-singularity pull explorer-env.img shub://qlan3/singularity-deffiles:explorer
+singularity pull explorer-env.img shub://qlan3/singularity-deffile:explorer
 
-singularity exec -bind /scratch/mzaheer/classic-control explorer-env.img bash Explorer/scripts/parallel_sun.sh
+singularity exec -bind /scratch/mzaheer/classic-control explorer-env.img bash Explorer/scripts/parallel_run.sh
