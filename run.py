5080d233b4672642c71031e9d58398a76378894e
qlan3 <qlan3@ualberta.ca>
2020-09-17 12:02:24 -0600

    update job submit

--- run.py
+++ run.py
@@ -1,39 +1,52 @@
 import os
 import sys
-import argparse
-
 from utils.submitter import Submitter
+
 
 def make_dir(dir):
   if not os.path.exists(dir): 
     os.makedirs(dir, exist_ok=True)
 
-def main(argv):
-  # python run.py --job_name lunar --script_path ./sbatch_lunar.sh
-  # python run.py --job_name copter --script_path ./sbatch_copter.sh
-  # python run.py --job_name catcher --script_path ./sbatch_catcher.sh
-  # python run.py --job_name minatar --script_path ./sbatch_minatar.sh
-  parser = argparse.ArgumentParser(description="Config file")
-  parser.add_argument('--user', type=str, default='qlan3', help='user name')
-  parser.add_argument('--job_name', type=str, default='minatar', help='job name')
-  parser.add_argument('--script_path', type=str, default='./sbatch.sh', help='sbatch script path')
-  parser.add_argument('--check_time_interval', type=int, default=10, help='check time interval in minutes')
-  args = parser.parse_args()
-  
-  cfg = dict()
-  # User name
-  cfg['user'] = args.user
-  # Sbatch script path
-  cfg['script_path'] = args.script_path
-  # Job indexes list
-  cfg['job_list'] = list(range(1,40*5+1))
-  # Check time interval in minutes
-  cfg['check_time_interval'] = args.check_time_interval
-  # cluster_name: cluster_capacity
-  cfg['clusters'] = {'Cedar': 9999}
 
-  make_dir(f'output/{args.job_name}')
-  submitter = Submitter(cfg)
+def main(argv):
+   
+  sbatch_cfg = {
+    # Account name
+    'account': 'rrg-whitem',
+    # Job name
+    'job-name': 'catcher',
+    # Job time
+    'time': '0-10:00:00',
+    # GPU/CPU type
+    '--cpus-per-task': 1,
+    # Memory
+    'mem-per-cpu': '2000M',
+    # Email address
+    'mail-user': 'qlan3@ualberta.ca'
+  }
+
+  # sbatch configs backup for different games
+  # sbatch_cfg['job-name'], sbatch_cfg['time'], sbatch_cfg['mem-per-cpu'] = 'catcher', '0-10:00:00', '2000M'
+  # sbatch_cfg['job-name'], sbatch_cfg['time'], sbatch_cfg['mem-per-cpu'] = 'copter', '0-05:00:00', '2000M'
+  # sbatch_cfg['job-name'], sbatch_cfg['time'], sbatch_cfg['mem-per-cpu'] = 'lunar', '0-07:00:00', '2000M'
+  # sbatch_cfg['job-name'], sbatch_cfg['time'], sbatch_cfg['mem-per-cpu'] = 'minatar', '1-08:00:00', '4000M'
+  
+  
+  general_cfg = {
+    # User name
+    'user': 'qlan3',
+    # Sbatch script path
+    'script-path': './sbatch.sh',
+    # Check time interval in minutes
+    'check-time-interval': 5,
+    # Clusters info: {name: capacity}
+    'clusters': {'Cedar': 3000},
+    # Job indexes list
+    'job-list': list(range(1, 30+1))
+   }
+
+  make_dir(f"output/{sbatch_cfg['job-name']}")
+  submitter = Submitter(general_cfg, sbatch_cfg)
   submitter.submit()
 
 if __name__=='__main__':
--- utils/submitter.py
+++ utils/submitter.py
@@ -3,12 +3,14 @@ import time
 
 
 class Submitter(object):
-  def __init__(self, cfg):
-    self.user = cfg['user']
-    self.script_path = cfg['script_path']
-    self.job_list = [str(x) for x in cfg['job_list']]
-    self.check_time_interval = cfg['check_time_interval'] * 60
-    self.clusters = cfg['clusters']
+  def __init__(self, general_cfg, sbatch_cfg):
+    self.user = general_cfg['user']
+    self.script_path = general_cfg['script-path']
+    self.job_list = [str(x) for x in general_cfg['job-list']]
+    self.check_time_interval = general_cfg['check-time-interval'] * 60
+    self.clusters = general_cfg['clusters']
+    self.general_cfg = general_cfg
+    self.sbatch_cfg = sbatch_cfg
   
   def submit_jobs(self, num_jobs, cluster_name):
     # Get job indexes and submit jobs
@@ -21,8 +23,8 @@ class Submitter(object):
       job_indexes = f'{self.job_list[0]}-{self.job_list[num_jobs-1]}'
     else:
       job_indexes = ','.join(self.job_list[:num_jobs])
-
-    bash_script = f'sbatch --array={job_indexes} {self.script_path}'
+    slurm_input = ' '.join([f'--{k}={v}' for k, v in self.sbatch_cfg.items()])
+    bash_script = f'sbatch --array={job_indexes} {slurm_input} {self.script_path}'
     myCmd = os.popen(bash_script).read()
     print(myCmd)
     print(f'Submit jobs from {self.job_list[0]} to {self.job_list[num_jobs-1]}')
@@ -39,7 +41,7 @@ class Submitter(object):
         for line in lines:
           if self.user in line:
             num_current_jobs += 1
-        print(f'Number of current running jobs in {cluster_name}:', num_current_jobs)
+        print(f'Number of current jobs in {cluster_name}:', num_current_jobs)
         # If cluster capacity is not reached and job list is not empty, submit new jobs
         if (num_current_jobs < cluster_capacity) and (len(self.job_list) > 0):
           num_jobs = min(cluster_capacity - num_current_jobs, len(self.job_list))
@@ -47,4 +49,4 @@ class Submitter(object):
           if len(self.job_list) == 0:
             print("Finish submitting all jobs!")
             exit(1)
-      time.sleep(self.check_time_interval)
+      time.sleep(self.check-time-interval)
